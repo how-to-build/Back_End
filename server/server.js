a70850b6fbb5fd87500d2c925d0e72c62c1ebe39
John O'Keefe <jokeefe@weegolobal.com>
2019-07-11 01:56:18 -0400

    added routes to server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
+const usersRoutes = require('./routes/users')
+const stepsRoutes = require('./routes/steps')
+const repliesRoutes = require('./routes/replies')
+const keyPointsRoutes = require('./routes/keyPoints')
+const howTosRoutes = require('./routes/howTos')
+const commentsRoutes = require('./routes/comments')
 
 const server = express()
 
 server.use(express.json())
 server.use(helmet())
 server.use(cors())
+server.use('/users', usersRoutes)
+server.use('/steps', stepsRoutes)
+server.use('/replies', repliesRoutes)
+server.use('/keypoints', keyPointsRoutes)
+server.use('/howTos', howTosRoutes)
+server.use('/comments', commentsRoutes)
 
 server.get('/', (req, res) => {
     res.json({ message: 'You reached the api'})
