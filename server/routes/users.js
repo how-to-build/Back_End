704fe0780a472c09ea647e0429bdbb4a8e0e71ad
iner Lofgren <iner.lofgren@gmail.com>
2019-07-10 21:40:01 -0700

    Users Route Built

---
+++ server/routes/users.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const router = express.Router()
+const db = require('../../data/model/usersModel')
+
+router.get('/', async (req, res) => {
+    try {
+        const allUsers = await db.find()
+        if(allUsers){
+            res.json({ message: "List of users"}, allUsers )
+        }else{
+            res.status(404).json({ message: "No users found"})
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" }, error)
+    }
+})
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+    
+    try {
+        const user = await db.findById(id)
+        if(user){
+            res.json({ message: `User with id of ${id}`}, user)
+        }else{
+            res.status(404).json({ message: "No user found with that id" })
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" }, error)
+    }
+})
+
+router.get('/:username', (req, res) => {
+    const { username } = req.params
+    try {
+        const user = await db.findByUser(username)
+        if(user){
+            res.json({ message: `User with username ${username}`}, user)
+
+        }else{
+            res.status(404).json({ message: `No user found with username: ${username}` }) 
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" }, error)
+    }
+})
+
+router.post('/', (req, res) => {
+    const user = req.body
+    try {
+        const newUser = await db.add(user)
+        if(newUser){
+            res.json({ message: `User: ${user.username} added successfully`}, newUser)
+        }else{
+            res.status(403).json({ message: "Bad request" })
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" }, error)
+    }
+})
+
+router.put('/', (req, res) => {
+    const { user }  
+    try {
+        const updatedUser = await db.update(user)
+        if(updatedUser){
+            res.json({ message: `Updated user: ${user.username}` })
+        }else{
+            res.status(403).json({ message: "Bad request" })
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" }, error)
+    }
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    try {
+        const user = await db.remove(id)
+        if(user){
+            res.json({ message: `User with id: ${id} removed` })
+        }else{
+            res.status(403).json({ message: `Bad request` })
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" }, error)
+    }
+})
+
+module.exports = router;
