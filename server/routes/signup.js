22d41e80b814bb1356ad9dddf76ddd8f68818bf1
iner Lofgren <iner.lofgren@gmail.com>
2019-07-15 21:56:34 -0700

    removed console log debug

--- server/routes/signup.js
+++ server/routes/signup.js
@@ -5,7 +5,6 @@ const router = express.Router();
 
 router.post("/", async (req, res) => {
   const credentials = req.body;
-  console.log("!!!!!!!!!!!!!!", credentials);
 
   try {
     if (credentials.username && credentials.password) {
